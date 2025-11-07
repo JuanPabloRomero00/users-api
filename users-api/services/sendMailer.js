52ed6155465cff8a7287811a0073f12035de9da6
Juan Pablo Romero <romerojuanpablo.00@gmail.com>
2025-11-07 14:07:57 -0300

    fix: correct createTransport typo (was createTransporter)

--- users-api/services/sendMailer.js
+++ users-api/services/sendMailer.js
@@ -2,7 +2,7 @@ const nodemailer = require('nodemailer');
 
 // Configuración específica para Gmail
 const createGmailTransporter = () => {
-  return nodemailer.createTransporter({
+  return nodemailer.createTransport({
     service: 'gmail',
     auth: {
       user: process.env.GMAIL_USER,
